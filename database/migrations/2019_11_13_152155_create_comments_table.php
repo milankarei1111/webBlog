7c5f746e31d5d2aad71c52d4ec24aabe609eb02a
wanru_tsai <milankarei1111@gmail.com>
2019-11-13 15:33:00 +0800

    add Model Comment add Migration create_comments_table

---
+++ database/migrations/2019_11_13_152155_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('comment_id');
+            $table->string('content');
+            $table->bigInteger('article_id');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
