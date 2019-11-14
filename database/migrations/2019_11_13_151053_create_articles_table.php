87c56192cbcdc1f141cf5a158e2417a4e932f4a8
wanru_tsai <milankarei1111@gmail.com>
2019-11-14 14:18:00 +0800

    bug modify :  use use Illuminate\Database\Eloquent\SoftDeletes;

--- database/migrations/2019_11_13_151053_create_articles_table.php
+++ database/migrations/2019_11_13_151053_create_articles_table.php
@@ -19,7 +19,7 @@ class CreateArticlesTable extends Migration
             $table->string('content');
             $table->bigInteger('category_id');
             $table->string('image');
-            $table->string('Remark');
+            $table->string('remark');
             $table->timestamps();
             $table->softDeletes();
         });
