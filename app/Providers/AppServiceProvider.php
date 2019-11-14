ce4e61a8dcd9e28064dc0cfb9170b0d8b76481f6
wanru_tsai <milankarei1111@gmail.com>
2019-11-14 11:54:41 +0800

    modify model ArticleCategory 引入 use Illuminate\Database\Eloquent\SoftDeletes; 調用填充:DatabaseSeeder調用ArticleCategoryTableSeeder填充 重新載入:composer dump_autoload 執行填充 :$ php artisan db:seed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,11 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // 綁定單例
-        $this->app->singleton(\Faker\Generator::class, function(){
-                return \Faker\Factory::create('zh-TW');
-            }
-         );
+        // 工廠中文化 綁定一個單例
+        $this->app->singleton(\Faker\Generator::class,function(){
+            return \Faker\Factory::create('zh_TW');
+        });
     }
 
     /**
