7d65edab2a1b30e5e028d9af515a5c3fec411cb9
wanru_tsai <milankarei1111@gmail.com>
2019-11-21 17:37:09 +0800

    新增登入功能

--- app/Http/Controllers/Auth/ApiController.php
+++ app/Http/Controllers/Auth/ApiController.php
@@ -15,6 +15,11 @@ class ApiController extends Controller
         $this->middleware('auth')->except('login', 'register');
     }
 
+    protected function username()
+    {
+        return 'name';
+    }
+
     public function register(Request $request)
     {
         $validator = $this->validator($request->all());
@@ -23,7 +28,7 @@ class ApiController extends Controller
             $value = $validator->errors();
         } else {
             $api_token = Str::random(80);
-            $data = \array_merge($request->all(), compact('api_token'));
+            $data = array_merge($request->all(), compact('api_token'));
             $result = $this->create($data);
 
             if ($result) {
@@ -37,12 +42,38 @@ class ApiController extends Controller
         return $this->responseMessage($status, $value);
     }
 
+    public function login()
+    {
+        $inputName = request($this->username());
+        $user = User::where($this->username(), $inputName)->get()->first();
+        if($user){
+            if(password_verify(request('password'), $user->password)){
+                $api_token = Str::random(80);
+                $result = $user->update(['api_token'=>hash('sha256', $api_token)]);
+                if ($result) {
+                    $status = '000000';
+                    $value = $api_token;
+                } else {
+                    $status = 'E00001';
+                    $value = '登入失敗';
+                }
+            } else {
+                $status = 'E00002';
+                $value = '輸入錯誤!';
+            }
+        } else {
+            $status = 'E00002';
+            $value = '輸入錯誤!';
+        }
+        return $this->responseMessage($status, $value);
+    }
+
     protected function create(array $data)
     {
         return User::forceCreate([
             'name' => $data['name'],
             // 'email'=>$data['email'],
-            'password' => \password_hash($data['password'], PASSWORD_DEFAULT),
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
             'api_token' => hash('sha256', $data['api_token'])
         ]);
     }
