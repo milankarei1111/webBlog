0a62e1c1abf3f2e4d9f6ddce15a2c7d9334f0c1b
wanru_tsai <milankarei1111@gmail.com>
2019-11-26 17:50:17 +0800

    調整路由api權限

--- routes/api.php
+++ routes/api.php
@@ -21,15 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'blog'], function () {
-    // API 資源類
-    // Route::get('category/{id?}', function($id=null){
-    //     $find = ArticleCategory::find($id);
-    //     if($find) {
-    //         return new CategoryResource($find);
-    //     } else {
-    //         return CategoryResource::collection(ArticleCategory::all());
-    //     }
-    // });
     Route::post('/register', 'Auth\ApiController@register');
     Route::post('/login', 'Auth\ApiController@login');
     Route::post('/refresh', 'Auth\ApiController@refresh');
@@ -37,18 +28,23 @@ Route::group(['prefix' => 'blog'], function () {
 
     Route::get('category/{id?}', 'API\CategoryController@categoryList');
     Route::get('category/{id}/article', 'API\CategoryController@articelList'); // 關聯文章
-    Route::patch('category/{id}', 'API\CategoryController@update');
-    Route::post('category', 'API\CategoryController@insert');
-    Route::delete('category/{id}', 'API\CategoryController@delete');
-
     Route::get('article/{id?}', 'API\ArticleController@articleList');
     Route::get('article/{id}/comment', 'API\ArticleController@commentList'); // 關聯評論
-    Route::match(['post', 'patch'],'article/{id}', 'API\ArticleController@update');
-    Route::post('article', 'API\ArticleController@insert');
-    Route::delete('article/{id}', 'API\ArticleController@delete');
-
     Route::get('comment/{id?}', 'API\CommentController@commentList');
-    Route::patch('comment/{id}', 'API\CommentController@update');
-    Route::post('comment', 'API\CommentController@insert');
-    Route::delete('comment/{id}', 'API\CommentController@delete');
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::patch('category/{id}', 'API\CategoryController@update');
+        Route::post('category', 'API\CategoryController@insert');
+        Route::delete('category/{id}', 'API\CategoryController@delete');
+
+        Route::match(['post', 'patch'],'article/{id}', 'API\ArticleController@update');
+        Route::post('article', 'API\ArticleController@insert');
+        Route::delete('article/{id}', 'API\ArticleController@delete');
+
+        Route::patch('comment/{id}', 'API\CommentController@update');
+        Route::post('comment', 'API\CommentController@insert');
+        Route::delete('comment/{id}', 'API\CommentController@delete');
+    });
 });
+
+
